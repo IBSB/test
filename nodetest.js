de982b1e4032b90cb8104b2be4c589fbd5cc8df6
IBSB <kuro4102@gmail.com>
2020-03-12 17:08:14 +0900

    win express test

--- nodetest.js
+++ nodetest.js
@@ -1,5 +1,13 @@
-var printFunc = function(name) {
-    console.log(name);
-}
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-printFunc('vscode & node.js');
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`server is listening at localhost:${process.env.PORT}`);
+});
