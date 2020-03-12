6dc2c19469d46490d176375c7204521c75f43016
IBSB <kuro4102@gmail.com>
2020-03-12 16:10:36 +0900

    mac js

--- nodetest.js
+++ nodetest.js
@@ -1,11 +1,5 @@
-var result = 0;
-
-console.time('duration_sum');
-
-for(var i=1;i<=1000000000;i++)
-{
-    result+=i;
+var printFunc = function(name) {
+    console.log(name);
 }
 
-console.timeEnd('duration_sum');
-console.log('1부터 1000까지 더한 결과물 :%d',result);
+printFunc('vscode & node.js');
