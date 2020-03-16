f1d620b57557f3098720747ec52f47753fbf9e16
IBSB <kuro4102@gmail.com>
2020-03-16 11:13:16 +0900

    install ncong

--- nodetest.js
+++ nodetest.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-var os = require('os');
-
-console.log(os.hostname);
-console.log(os.freemem);
-console.log(os.totalmem);
-console.log(os.cpus);
-console.log(os.networkInterfaces);
-=======
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,4 +11,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`server is listening at localhost:${process.env.PORT}`);
 });
->>>>>>> de982b1e4032b90cb8104b2be4c589fbd5cc8df6
